3d4cf232ad34a8ba8d69662baade888e14ff1e75
sandro-1 <mijatovic.sandro@gmail.com>
2022-03-17 15:47:00 -0700

    Second commit.

--- Github1/Program.cs
+++ Github1/Program.cs
@@ -6,7 +6,7 @@ namespace Github1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fuck Github!");
         }
     }
 }
